192e16c93d002e373bda9025d3468197a70fbb04
Dave Tarrant <davetaz@theodi.org>
2018-02-18 18:33:48 +0000

    Update

--- js/encrypted.js
+++ js/encrypted.js
@@ -1,10 +1,8 @@
 // INIT
 //var myPassword = "sdsadsa";
-
+// From https://codepen.io/gabrielizalo/pen/oLzaqx
 $( "#submit" ).click(function() {
   var myPassword = $("#passkey").val();
-  console.log('hello');
-  console.log(myPassword);
   $.get('info.enc.html', function(data) {
   	var decrypted = CryptoJS.AES.decrypt(data, myPassword);
   	document.getElementById("main").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
