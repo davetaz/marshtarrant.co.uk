1a8c8c2e50a4080b9fc629364e02ebbc26173fe1
Dave Tarrant <davetaz@theodi.org>
2018-02-21 09:06:56 +0000

    Updating

--- js/encrypted.js
+++ js/encrypted.js
@@ -1,10 +1,44 @@
 // INIT
-//var myPassword = "sdsadsa";
 // From https://codepen.io/gabrielizalo/pen/oLzaqx
+$( document ).ready(function() {
+	if (localStorage.getItem('CMPassKey')) {
+		var myPassword = localStorage.getItem('CMPassKey');
+		unlockPage(myPassword);
+	}
+	/*
+	$.get('info2.raw.html', function(data) {
+		console.log('here');
+		var encrypted = CryptoJS.AES.encrypt(data, "");
+  		//var decrypted = CryptoJS.AES.decrypt(data, myPassword);
+  		document.getElementById("main").innerHTML = encrypted;
+  	});
+  	*/
+});
+
 $( "#submit" ).click(function() {
   var myPassword = $("#passkey").val();
-  $.get('info.enc.html', function(data) {
-  	var decrypted = CryptoJS.AES.decrypt(data, myPassword);
-  	document.getElementById("main").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
-  });
+  localStorage.setItem("CMPassKey",myPassword);
+  unlockPage(myPassword);
 });
+
+function unlockPage(myPassword) {
+  $.get("info.enc.html", function(data) {
+  	try {
+  		var decrypted = CryptoJS.AES.decrypt(data, myPassword);
+  		var output = decrypted.toString(CryptoJS.enc.Utf8);
+  		document.getElementById("main").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+  	} catch (err) {
+  		$.get("info2.enc.html", function(data) {
+  			try {
+  				var decrypted = CryptoJS.AES.decrypt(data, myPassword);
+  				var output = decrypted.toString(CryptoJS.enc.Utf8);
+  				document.getElementById("main").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+  			} catch (err) { localStorage.removeItem("CMPassKey"); }
+  		});
+  	}
+  });
+}
+
+
+
+  
